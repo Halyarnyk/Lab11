1407933188c741c800e157240adf335e02574b2a
Halyarnyk <galyarnyk102@gmail.com>
2021-03-30 21:31:28 +0300

    Lab_11
    KPZ

---
+++ Lab 9/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Postavte bud' laska zalik :) ");
+            Console.ReadKey();
+        }   
+    }
+}
